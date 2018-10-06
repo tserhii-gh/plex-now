528d01c82f8f5edffddcad0d5e5182d13dd8e78a
Sergij Cy`najlo <funeral20@gmail.com>
2018-10-06 17:11:15 +0300

    Added progress bar

--- plex-now.py
+++ plex-now.py
@@ -14,28 +14,57 @@ notification_text = ''
 error_color = '#f4b042'
 
 
+def calculate_lenght(*args):
+    line_length = 0
+    for arg in args:
+        line_length += len(arg)
+    return line_length
+
+
 def send_notification(title, text, full_path_to_icon=''):
     Notify.init('PlexNow')
     n = Notify.Notification.new(title, text, full_path_to_icon)
     n.show()
 
 
+def progress(count, total, bar_len=20):
+    filled_len = int(round(bar_len * count / float(total)))
+    # percents = round(100.0 * count / float(total), 1)
+    bar = '▰' * filled_len + '▱' * (bar_len - filled_len)
+    # bar = '⣿' * filled_len + '⣀' * (bar_len - filled_len)
+    # bar = '▮' * filled_len + '▯' * (bar_len - filled_len)
+    return bar
+
+
 def notify_text_builder(item):
     try:
         if item['type'] == 'movie':
-            return '<b><span color=\'{}\'>{}</span> ({})</b>'.format(
+            total_length = calculate_lenght(item['title'],
+                                            item['year'])
+            text = '<b><span color=\'{}\'>{}</span> ({})</b>'.format(
                 error_color,
                 item['title'],
                 item['year']
             )
+            text += '\n<span color=\'#cc7b19\'>' + progress(int(item['viewOffset']), int(
+                    item['Media']['Part']['duration']), total_length) + '</span>'
+            return text
         elif item['type'] == 'episode':
-            return '<b><span color=\'{}\'>{}</span></b> › <b>{}</b> › <b><span color=\'{}\'>{}</span></b>'.format(
+            total_length = calculate_lenght(
+                item['grandparentTitle'].title(),
+                item['parentTitle'],
+                item['title'])
+            text = '<b><span color=\'{}\'>{}</span></b> › <b>{}</b> › <b><span color=\'{}\'>{}</span></b>'.format(
                 error_color,
                 item['grandparentTitle'].title(),
                 item['parentTitle'],
                 error_color,
                 item['title'])
-    except KeyError as e:
+            text += '\n' + progress(int(item['viewOffset']), int(
+                    item['Media']['Part']['duration']), total_length)
+            return text
+
+    except (KeyError, IndexError) as e:
         return '<b><span color=\'{}\'>No such Key: {}</span></b>'.format(
             error_color,
             e)
@@ -50,6 +79,6 @@ else:
     notification_text = '<b>No media playing</b>'
     print(notification_text)
 
-send_notification("PlexNow", notification_text, icon)
+send_notification("PlexNow", '\n' + notification_text, icon)
 
 Notify.uninit()
